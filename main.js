aed7611fd1724b5c56c80e54cdd719ebc664f872
JWest11 <114183436+JWest11@users.noreply.github.com>
2022-11-22 15:21:41 -0700

    undate carousel

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ class Carousel {
     renderController() {
         const element = document.getElementById(this.controllerId);
         element.innerHTML = `
-        <button class="carouselButton" id="prev">
+        <button class="carouselButton" id="next">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <line x1="5" y1="12" x2="19" y2="12" />
@@ -48,7 +48,7 @@ class Carousel {
                 <line x1="5" y1="12" x2="11" y2="6" />
             </svg>
         </button>
-        <button class="carouselButton" id="next">
+        <button class="carouselButton" id="prev">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <line x1="5" y1="12" x2="19" y2="12" />
